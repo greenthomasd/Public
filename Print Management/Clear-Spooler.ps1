7cb5f562d8d3d486d2aeaa1fa5a3e4af6b10a4cd
Thomas Green <55002650+greenthomasd@users.noreply.github.com>
2019-11-19 16:13:41 -0600

    Clear Spooler publish

---
+++ Print Management/Clear-Spooler.ps1
@@ -0,0 +1,21 @@
+﻿#PrintServer has a dependency on the print spooler service, you must stop the PaperCut Print Provider service (PCPrintProvider) before stopping the spooler service, and after the Print Spooler service starts. 
+$Computer = "PrintServerFQDN"
+
+Invoke-Command -ComputerName $Computer -ScriptBlock {
+
+#Stop the PaperCut Print Provider service
+get-service PCPrintProvider | Stop-Service -force
+
+#Stop the Spooler service
+get-service Spooler | Stop-Service -force
+
+#Delete the files from the Spooler cache
+remove-item C:\Windows\system32\spool\Printers\* -recurse
+
+#Start the Spooler Service
+get-service Spooler | Start-Service
+
+#Start the PaperCut Print Provider service
+get-service PCPrintProvider | Start-Service
+
+}
