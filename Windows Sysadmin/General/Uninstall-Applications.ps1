eace1b68687ce54998678f96379ebbe64754b74f
Thomas Green <55002650+greenthomasd@users.noreply.github.com>
2019-11-19 12:58:11 -0600

    Uninstall-Applications

---
+++ Windows Sysadmin/General/Uninstall-Applications.ps1
@@ -0,0 +1,6 @@
+﻿$computers = "PCName"
+
+Invoke-Command -ComputerName $computers -ScriptBlock {
+
+Get-WmiObject -Class win32_product -Filter "Name like 'Dell OpenManage'" | ForEach-Object { $_.Uninstall()}
+}
