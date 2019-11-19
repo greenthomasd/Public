9795d5ede409d53ef35aa1220c291a5044cefc12
Thomas Green <55002650+greenthomasd@users.noreply.github.com>
2019-11-19 16:41:35 -0600

    Get Powered Off VMs Script

---
+++ VMware/vCenter/Get-PoweredOffVMs.ps1
@@ -0,0 +1,19 @@
+﻿#Get-PoweredOffVMs
+
+#vCenter FQDN
+$vCenter = "vCenterFQDN"
+
+#Connect to vCenter through PowerCLI
+Connect-VIServer $vCenter
+
+#VM query
+Get-VM * | Where-Object -Property PowerState -EQ PoweredOff | 
+
+#Select Properties
+Select-Object -Property Name, Notes, Guest, Folder, UsedSpaceGB, VMHost | 
+
+#Export CSV to local user's downloads directory 
+Export-Csv $env:USERPROFILE\downloads\PoweredOffVMs_$($vCenter)_$((Get-Date).ToString('MM-dd-yyyy')).csv -Verbose
+
+#Disconnect from vCenter
+Disconnect-VIServer $vCenter -Confirm:$false -Verbose
