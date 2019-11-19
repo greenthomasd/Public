134e94f3cdc21742c285b45a08d09e63543b96f2
Thomas Green <55002650+greenthomasd@users.noreply.github.com>
2019-11-19 13:02:08 -0600

    Get-Local-Admins

---
+++ Windows Sysadmin/General/Get-Local-Admins.ps1
@@ -0,0 +1,59 @@
+﻿#This will return a list of groups & users who are in the local administators group on an array of computers
+
+#Define Computer Array - AD Query for all computer objects with the name "Windows Server"
+$Computers = Get-ADComputer -Filter {OperatingSystem -Like '*Windows Server*'} 
+
+#https://powershell.org/2013/04/02/get-local-admin-group-members-in-a-new-old-way-3/
+Function Get-NetLocalGroup {
+[cmdletbinding()]
+
+Param(
+[Parameter(Position=0)]
+[ValidateNotNullorEmpty()]
+[object[]]$Computername=$env:computername,
+[ValidateNotNullorEmpty()]
+[string]$Group = "Administrators",
+[switch]$Asjob
+)
+
+Write-Verbose "Getting members of local group $Group"
+
+#define the scriptblock
+$sb = {
+ Param([string]$Name = "Administrators")
+$members = net localgroup $Name | 
+ Where-Object {$_ -AND $_ -notmatch "command completed successfully"} | 
+ Select-Object -skip 4
+New-Object PSObject -Property @{
+ Computername = $env:COMPUTERNAME
+ Group = $Name
+ Members=$members
+ }
+} #end scriptblock
+
+#define a parameter hash table for splatting
+$paramhash = @{
+ Scriptblock = $sb
+ HideComputername=$True
+ ArgumentList=$Group
+ }
+
+if ($Computername[0] -is [management.automation.runspaces.pssession]) {
+    $paramhash.Add("Session",$Computername)
+}
+else {
+    $paramhash.Add("Computername",$Computername)
+}
+
+if ($asjob) {
+    Write-Verbose "Running as job"
+    $paramhash.Add("AsJob",$True)
+}
+
+#run the command
+Invoke-Command @paramhash | Select-Object * -ExcludeProperty RunspaceID
+
+} #end Get-NetLocalGroup
+
+#Run function, output to the logged in user's documents directory
+$Computers.name |  Get-NetLocalGroup > "$env:USERPROFILE\Documents\ServersAdminList.txt"
