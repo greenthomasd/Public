1ed920e7147cb4fddd75774ce203bfd5790ab9b4
Thomas Green <55002650+greenthomasd@users.noreply.github.com>
2019-11-19 16:10:11 -0600

    Publish CommVault Scripts

---
+++ CommVault/CommVault Servers Drive Space.ps1
@@ -0,0 +1,11 @@
+﻿#Show the available drive space on the CommVault Infrastrucure Servers
+
+$CommVaultServers = Get-ADGroupMember "Distinguished Name of CommVault Computer Security Group"
+
+foreach ($CommVaultServers in $CommVaultServers){ 
+Get-PSDrive 
+}
+
+#Invoke-Command -ComputerName $CommVaultServers -ScriptBlock {
+#Get-PSDrive | Format-Table -GroupBy PSComputerName
+#}
---
+++ CommVault/Restart CommVault Services.ps1
@@ -0,0 +1,8 @@
+﻿#Remotely Restart CommVault Services on CommVault Media Agents
+
+$CommVaultServers = Get-ADGroupMember "Distinguished Name of CommVault Media Agent Security Group"
+
+foreach ($CommVaultServer in $CommVaultServers) {
+
+    Invoke-Command -ComputerName $CommVaultServer -ScriptBlock { Get-Service gx* | Where-Object Status -EQ "running" | Restart-Service -Force -PassThru }
+}
