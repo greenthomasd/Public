35c5b7d631ce11f6b556dfdb30741223d3902c9c
Thomas Green <55002650+greenthomasd@users.noreply.github.com>
2019-11-19 16:27:22 -0600

    Change ESXi root password

---
+++ VMware/vCenter/Change-ESXiPassword.ps1
@@ -0,0 +1,24 @@
+﻿#This script is useful if you know the root password on the ESXi hosts that you'd like to change.
+#The host will need to be put into maintenance mode and rebooted prior to the new password working. 
+
+#vCenter FQDN
+$vCenter = "vCenterFQDN"
+
+#Connect to vCenter through PowerCLI
+Connect-VIServer $vCenter -Verbose #-Credential $Creds
+
+$VMhosts = Get-VMHost #-Server $vc |Sort Name
+
+foreach ($VMhost in $VMhosts) {
+  
+  #OriginalESXiHostPassword is a placeholder 
+  Connect-VIServer $VMhost -User root -Password OriginalESXiHostPassword
+  
+  #NewESXiHostPassword is a placeholder 
+  Set-VMHostAccount –UserAccount root –Password NewESXiHostPassword
+}
+
+exit
+
+#Disconnect from vCenter
+Disconnect-VIServer $vCenter -Confirm:$false -Verbose
