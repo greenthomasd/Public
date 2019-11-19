fcbf64820d8b936dbc0b8992802de26bcc76fcad
Thomas Green <55002650+greenthomasd@users.noreply.github.com>
2019-11-19 12:51:12 -0600

    Delete-Remote-WU-Cache Commit

---
+++ Windows Sysadmin/General/Delete-Remote-WU-Cache.ps1
@@ -0,0 +1,16 @@
+﻿#This script is designed to delete the Windows Update cache on remote computers. 
+
+#This variable does work with multiple computer names by using commas to seperate the names. Example "pc1", "pc2", "pc3", etc... (Must use quotes around names)
+$ComputerName = "PCNameHere"
+
+Invoke-Command -ComputerName $ComputerName -ScriptBlock { 
+
+#Stop Windows Update Service
+Stop-Service wuauserv -Force -PassThru 
+
+#Remove Windows Updates local cache location
+Remove-Item "C:\Windows\SoftwareDistribution\*" -Recurse
+
+#Start Windows Update Service
+Start-Service wuauserv -PassThru
+}
