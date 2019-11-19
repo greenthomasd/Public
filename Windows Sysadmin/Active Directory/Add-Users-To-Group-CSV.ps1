9eae39d743e32933e3ea9bfc7398af1950b1a1ec
Thomas Green <55002650+greenthomasd@users.noreply.github.com>
2019-11-19 16:16:21 -0600

    Add users to group from CSV

---
+++ Windows Sysadmin/Active Directory/Add-Users-To-Group-CSV.ps1
@@ -0,0 +1,15 @@
+﻿#Source URL : https://emeneye.wordpress.com/2014/07/09/adding-multiple-users-to-a-group-in-active-directory-using-powershell-and-csv/
+
+# Import active directory module for running AD cmdlets
+Import-module ActiveDirectory
+
+#Store the data from UserList.csv in the $List variable
+$List = Import-CSV .\UserList.csv
+
+#Loop through user in the CSV
+ForEach ($User in $List)
+{
+
+#Add the user to the TestGroup1 group in AD
+Add-ADGroupMember -Identity TestGroup1 -Member $User.username
+}
