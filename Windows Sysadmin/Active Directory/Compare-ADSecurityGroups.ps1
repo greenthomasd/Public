73619607bd3fa56a4fde26b83a69f0d853eb6b99
Thomas Green <55002650+greenthomasd@users.noreply.github.com>
2019-11-19 16:20:54 -0600

    Compare AD Security Group publish

---
+++ Windows Sysadmin/Active Directory/Compare-ADSecurityGroups.ps1
@@ -0,0 +1,12 @@
+﻿#Compare 2 AD Security Group for Members
+
+#Reference Article : https://4sysops.com/archives/compare-active-directory-group-membership-with-powershell/
+
+#First Security Group (FSG) Variable
+$FSG =  Get-ADGroupMember "distinguishedGroup1"
+
+#Second Security Group (SSG) Variable
+$SSG = Get-ADGroupMember "distinguishedGroup2"
+
+#Find members in the FSG that are not in the SSG, sort the output, publish it to the user's document folder
+$FSG.name | Where-Object { $SSG.name -notcontains $PSItem } | Sort-Object | Out-File "$env:USERPROFILE\Documents\ADSecurityGroupComparision.txt"
